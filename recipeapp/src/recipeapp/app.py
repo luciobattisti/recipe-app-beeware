7e4c872e57b69436cd383a6cd5766f4d31cdc42f
Lucio Battisti <al.lusci@yahoo.com>
2024-05-03 18:51:40 -0400

    Include menu and add/delete option

--- recipeapp/src/recipeapp/app.py
+++ recipeapp/src/recipeapp/app.py
@@ -86,9 +86,41 @@ class RecipeApp(toga.App):
         if not Path(selections_fpath).is_file():
             shutil.copyfile(selections_res_fpath, selections_fpath)
 
-        print(f"{self.paths.data}/{db_fname}")
         self.db_helper = SQLiteHelper(db_fpath)
         self.selections_fpath = selections_fpath
+
+        with open(self.selections_fpath) as fp:
+            self.saved_selections = json.load(fp)
+
+        # Create Main Box
+        main_box = self.create_main_box()
+
+        # Menu 
+        options = toga.Group("Menu")
+        opt_add_recipe_box = toga.Command(
+            self.show_add_recipe_box,
+            text="Add/Delete Recipe",
+            tooltip="Show Add Recipe Window",
+            group=options,
+        )
+
+        opt_main_box = toga.Command(
+            self.show_main_box,
+            text="Main",
+            tooltip="Show main box",
+            group=options
+        )
+        
+        self.commands.add(opt_add_recipe_box)
+        self.commands.add(opt_main_box)
+        
+        # Define main window
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+
+        self.main_window.show()
+
+    def create_main_box(self) -> toga.Box:
 
         with open(self.selections_fpath) as fp:
             self.saved_selections = json.load(fp)
@@ -126,8 +158,8 @@ class RecipeApp(toga.App):
         main_box.add(self.selected_table)
 
         # Add populate cart button box
-        self.populate_cart = self.get_populate_cart_button_box()
-        main_box.add(self.populate_cart)
+        self.populate_cart_button = self.get_populate_cart_button_box()
+        main_box.add(self.populate_cart_button)
 
         # Add label
         selected_recipes_label =  toga.Label(
@@ -149,11 +181,97 @@ class RecipeApp(toga.App):
         self.load_button = self.get_load_button_box()
         main_box.add(self.load_button)
 
-        # Define main window
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        return main_box
 
-        self.main_window.show()
+    def show_add_recipe_box(self, widget):
+        self.main_window.content.clear()
+
+        self.ingredients_list = self.get_ingredient_list()
+
+        add_recipe_box = toga.Box(style=Pack(direction=COLUMN))
+
+        # Add Recipe Name label
+        recipe_name_label =  toga.Label(
+            "Recipe Name", 
+            style=Pack(text_align=CENTER)
+        )
+        add_recipe_box.add(recipe_name_label)
+
+        # Add Recipe Name input 
+        self.recipe_name_input = toga.TextInput(
+            style=Pack(
+                padding=5
+            )
+        )
+        add_recipe_box.add(self.recipe_name_input)
+
+        # Add "Add Ingredient" label
+        add_ingredient_label =  toga.Label(
+            "Ingredients", 
+            style=Pack(text_align=CENTER)
+        )
+        add_recipe_box.add(add_ingredient_label)
+
+        # Add recipe ingredient table
+        self.recipe_ingredient_table = self.get_recipe_ingredient_table()
+        add_recipe_box.add(self.recipe_ingredient_table)
+
+        # Add ingredient input 
+        ingredient_input = toga.TextInput(
+            placeholder="Refine Ingredient Search",
+            style=Pack(
+                padding=5
+            ),
+            on_change=self.update_ingredients_list
+        )
+        add_recipe_box.add(ingredient_input)
+
+        # Add ingredient selection 
+        self.ingredient_selection = self.get_ingredient_selection_box()
+        add_recipe_box.add(self.ingredient_selection)
+
+        # Add quantity box
+        self.quantity_box = toga.NumberInput(
+            style=Pack(
+                padding=5
+            ),
+            min_value=1,
+            value=1
+        )
+
+        add_recipe_box.add(self.quantity_box)
+
+        # Add unit selection box
+        self.unit_selection = toga.Selection(
+            items=self.units.keys(),
+            accessor="name",
+            style=Pack(
+                padding=5
+            ),
+        )
+
+        add_recipe_box.add(self.unit_selection)
+
+        # Include Add Ingredient button
+        self.add_ingredient_button = self.get_add_ingredient_button()
+        add_recipe_box.add(self.add_ingredient_button)
+
+        # Include Add Recipe button
+        save_recipe_button = self.get_save_recipe_button()
+        add_recipe_box.add(save_recipe_button)
+
+        # Include Delete Recipe button
+        delete_recipe_button = self.get_delete_recipe_button()
+        add_recipe_box.add(delete_recipe_button)
+
+        self.main_window.content = add_recipe_box
+
+    def show_main_box(self, widget):
+
+        self.main_window.content.clear()
+        
+        main_box = self.create_main_box()
+        self.main_window.content = main_box
 
     def get_recipe_selection_box(self):
 
@@ -247,13 +365,12 @@ class RecipeApp(toga.App):
             value=value,
             style=Pack(
                 padding=5,
-                height=100
+                height=125
             )
         )
 
         return additional_items_box
     
-
     def remove_additional_items(self, widget, row):
 
         pass     
@@ -383,8 +500,177 @@ class RecipeApp(toga.App):
                 
 
         return data
+    
+    def get_ingredient_selection_box(self):
+        
+        selection_box = toga.Selection(
+            items=self.ingredients_list, # We may need to place this in a better place
+            accessor="name",
+            style=Pack(
+                padding=5
+            )
+        )
+        
+        return selection_box
 
+    def get_ingredient_list(self):
 
+        ingredients = [x[1] for x in self.db_helper.get_all_ingredients()]
+
+        return ingredients
+    
+    def update_ingredients_list(self, widget):
+
+        search_str = widget.value.lower()
+
+        curr_ingredients = [x for x in self.ingredients_list if search_str in x]
+
+        self.ingredient_selection.items = curr_ingredients
+
+    def get_recipe_ingredient_table(self, ingredients=[]):
+
+            table_box = toga.Table(
+                headings=["Name", "Quantity", "Unit"],
+                data = ingredients,
+                on_activate=self.remove_ingredient_from_recipe,
+                style=Pack(
+                    padding=5,
+                    height=150
+                )
+            )
+
+            return table_box
+    
+    def remove_ingredient_from_recipe(self, widget, row):
+
+        self.recipe_ingredient_table.data.remove(row)
+    
+    def get_add_ingredient_button(self):
+            
+        button_box = toga.Button(
+            "Add Ingredient",
+            on_press=self.add_ingredient,
+            style=Pack(
+                padding=5
+            )
+        )
+
+        return button_box
+
+    def add_ingredient(self, widget):
+
+        ingredient = (
+            self.ingredient_selection.value.name,
+            self.quantity_box.value,
+            self.unit_selection.value.name            
+        )
+
+        self.recipe_ingredient_table.data.append(ingredient)
+
+    def get_save_recipe_button(self):
+
+        button_box = toga.Button(
+            "Save Recipe",
+            on_press=self.save_recipe,
+            style=Pack(
+                padding=5
+            )
+        )
+
+        return button_box
+
+    def save_recipe(self, widget):
+
+        ingredients = []
+        for item in self.recipe_ingredient_table.data:
+            ingredients.append(
+                {
+                    "name": item.name,
+                    "quantity": item.quantity,
+                    "unit": item.unit
+                }
+            )
+
+        recipe_name = self.recipe_name_input.value
+        if not recipe_name:
+            self.main_window.info_dialog(
+                "Invalid Recipe Name",
+                "Please enter a valid recipe name",
+            )
+        
+            return
+        
+        if not ingredients:
+            self.main_window.info_dialog(
+                "No Ingredients",
+                "Please add at least 1 ingredient",
+            )
+
+            return
+
+        success = self.db_helper.add_recipe(
+                self.recipe_name_input.value.lower(), 
+                ingredients
+            )
+        
+        if success:
+            self.main_window.info_dialog(
+            "Success",
+            f"Recipe {recipe_name} added successfully",
+        )
+        else:
+            self.main_window.info_dialog(
+                "Error",
+                "Could not save recipe. Make sure that the name hasn't been already used"
+            )
+
+    def get_delete_recipe_button(self):
+
+        button_box = toga.Button(
+            "Delete Recipe",
+            on_press=self.delete_recipe,
+            style=Pack(
+                padding=5
+            )
+        )
+
+        return button_box
+
+    def delete_recipe(self, widget):
+
+        recipe_name = self.recipe_name_input.value.lower()
+        if not recipe_name:
+            self.main_window.info_dialog(
+                "Invalid Recipe Name",
+                "Please enter a valid recipe name",
+            )
+
+            return
+        
+        recipe_id = self.db_helper.get_recipe_id(recipe_name)
+
+        if not recipe_id:
+            self.main_window.info_dialog(
+                "Error",
+                f"Cannot find {recipe_name} in database",
+            )
+
+            return
+
+        success = self.db_helper.delete_recipe(recipe_name)
+
+        if success:
+            self.main_window.info_dialog(
+                "Success",
+                f"Recipe {recipe_name} DELETED successfully",
+            )
+        else:
+            self.main_window.info_dialog(
+                "Error",
+                "Could not delete recipe. Make sure that the recipe name exists"
+            )
+
+        
 def main():
     return RecipeApp()
 
